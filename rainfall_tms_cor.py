b2cb46149eba1dac2a7e9e20f989b96bc2404545
pppppenny <molipenny@yahoo.com>
2025-07-03 17:13:20 +0100

    create file and set up

---
+++ rainfall_tms_cor.py
@@ -0,0 +1,14 @@
+## Time series analysis for rainfall data in Bangladesh 
+
+# imports
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import statistics
+from scipy.stats import linregress
+from matplotlib.backends.backend_pdf import PdfPages
+import glob
+import os
+
+
